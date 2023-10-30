537949a14af22f9d30e889116d9bd11af4112095
seonmikwak <sunmi1220ks@naver.com>
2023-10-30 17:10:00 +0900

    React 이미지 크기 변경 문제

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Exam01 from './components/Exam01';
+import Exam02 from './components/Exam02';
 
 function App() {
   return (
@@ -8,8 +9,11 @@ function App() {
       <h1>각종 리액트 예제</h1>
       <hr/>
       <Exam01/>
+      <hr/>
+      <Exam02/>
     </>
     );
 }
+
 
 export default App;
