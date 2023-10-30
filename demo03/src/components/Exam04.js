56c96a12aba8fdcfa96b065f8179c2f920fcf32b
seonmikwak <sunmi1220ks@naver.com>
2023-10-30 19:04:59 +0900

    주말에 뭐하세요? textLength 예제

--- demo03/src/components/Exam04.js
+++ demo03/src/components/Exam04.js
@@ -6,10 +6,14 @@ function Exam04(){
 
     return(
         <>
-            <h1>(Q)주말에 뭐하세요?</h1>
-            <textarea className="w-100" rows="6" maxLength="1000"onChange={(e)=>setLength(e.target.textLength)}></textarea>
-            <div className="text-end">
-                <sapn>{length}</sapn>/1000 btyes
+            <div className="row">
+                <div className="col-8 offset-2">
+                    <h1 className="card text-white bg-info">(Q)주말에 뭐하세요?</h1>
+                    <textarea className="form-label bg-light w-100" rows="6" maxLength="1000"onChange={(e)=>setLength(e.target.textLength)}></textarea>
+                    <div className="text-end">
+                    <sapn>{length}</sapn>/1000 btyes
+                </div>
+            </div>
             </div>
             
             
