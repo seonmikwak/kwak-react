9e764068e78adec9d17c5e934c2b17b799563d93
seonmikwak <sunmi1220ks@naver.com>
2023-10-31 13:13:34 +0900

    상태를 객체로 관리하는 예제

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -4,7 +4,8 @@ import Exam01 from './components/Exam01';
 import Exam02 from './components/Exam02';
 import Exam03 from './components/Exam03';
 import Exam04 from './components/Exam04';
-import Exam05 from './components/Exam05,js';
+import Exam05 from './components/Exam05';
+import Exam06 from './components/Exam06';
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
       <Exam04/>
       <hr/>
       <Exam05/>
+      <hr/>
+      <Exam06/>
     </>
     );
 }
