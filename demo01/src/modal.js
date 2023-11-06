a283dd0509db9a28be511bd53dca2977a58f799b
seonmikwak <sunmi1220ks@naver.com>
2023-11-06 18:17:24 +0900

    몬스터 등록 구현 중

---
+++ demo01/src/modal.js
@@ -0,0 +1,20 @@
+{/* Modal */}
+<div className="modal fade" ref={bsModal} 
+          data-bs-backdrop="static" tabIndex="-1" role="dialog" aria-hidden="true">
+  <div className="modal-dialog" role="document">
+      <div className="modal-content">
+      <div className="modal-header">
+          <h5 className="modal-title" >제목</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+          </button>
+      </div>
+      <div className="modal-body">
+          
+      </div>
+      <div className="modal-footer">
+          
+      </div>
+      </div>
+  </div>
+</div>
