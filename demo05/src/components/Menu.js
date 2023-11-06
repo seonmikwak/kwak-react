77b1aebaf9e31607bb3dd3fd38116b52e1e87c1e
seonmikwak <sunmi1220ks@naver.com>
2023-11-06 15:21:21 +0900

    포켓몬스터 목록 서버와 연동하여 출력

--- demo05/src/components/Menu.js
+++ demo05/src/components/Menu.js
@@ -7,7 +7,7 @@ const Menu = props=>{
 
   return(
     <>
-      <nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+      <nav className="navbar navbar-expand-lg bg-light fixed-top" data-bs-theme="light">
         <div className="container-fluid">
           <NavLink className="navbar-brand" to="/">앱제목</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
