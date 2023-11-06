69e67302f3c405b0e316614b20939712db2de986
seonmikwak <sunmi1220ks@naver.com>
2023-11-06 14:19:58 +0900

    메뉴 디자인 변경

--- demo05/src/App.js
+++ demo05/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink , Route, Routes } from "react-router-dom";
+import Pocketmon from "./components/Pocketmon";
+import Book from "./components/Book";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      {/* 상단 메뉴 영역 */}
+      <Menu/>
+
+      {/* 본문 영역 */}
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route path="/pocketmon" element={<Pocketmon/>}></Route>
+          <Route path="/book" element={<Book/>}></Route>
+        </Routes>
+      </div>
+
     </div>
   );
 }
---
+++ demo05/src/components/Menu.js
@@ -0,0 +1,36 @@
+import { NavLink, useLocation } from "react-router-dom";
+import "bootstrap/dist/js/bootstrap.js";
+
+const Menu = props=>{
+  const location = useLocation();
+  // console.log(location.pathname);
+
+  return(
+    <>
+      <nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">앱제목</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor03">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink className={`nav-link ${location.pathname === '/pocketmon' ? 'active' : ''}`} to="/pocketmon">포켓몬스터</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={`nav-link ${location.pathname === '/book' ? 'active' : ''}`} to="/book">도서</NavLink>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-sm-2" type="search" placeholder="Search"/>
+              <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Menu;
